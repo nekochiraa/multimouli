0a9dd255b5513a8a8c9dcf6382a9637027d44a20
Julien Guiauchain <julien.guiauchain@r04p03.lv323.lyn-vai.sm.cri.epita.fr>
2026-01-30 16:36:08 +0100

    augustrain le nullos

--- src/main.ml
+++ src/main.ml
@@ -4,3 +4,7 @@ open String
 let main = match Sys.argv with
   |[Sys.executable_name]|Sys.executable_name:: _ -> raise ("invalid arg") 
 let writefile file string = sys.command "echo"^string^" > "^"file";;
+let get key = function
+        | Yaml.O fields -> List.assoc_opt key fields
+        | _ -> None;;
+
